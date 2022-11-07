9c88742b50e2e055dcd872e02f9a449246e45269
amir rapaport <amir722@gmail.com>
2022-11-07 11:04:40 +0200

    Useing getStaticProps

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -13,4 +13,40 @@ function MeetupDetailsPage() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "m1",
+        },
+      },
+      {
+        params: {
+          meetupid: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupid = context.params;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+        id: meetupid,
+        title: "A First Meetup",
+        address: "Some addewss 5, 12345 City",
+        description: "This is a Meetup Description",
+      },
+    },
+    revalidate: 10,
+  };
+}
+
 export default MeetupDetailsPage;
